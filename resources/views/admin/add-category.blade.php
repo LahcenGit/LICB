bed218b66a1ddbffc66ed0a556699ead078f27a3
HindBenos <62807209+HindBenos@users.noreply.github.com>
2022-03-22 10:42:30 +0100

    categories

---
+++ resources/views/admin/add-category.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.dashboard-admin')
+@section('content')
+
+<div class="content-body">
+            <div class="container-fluid">
+				<div class="row page-titles">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item active"><a href="javascript:void(0)">Form</a></li>
+						<li class="breadcrumb-item"><a href="javascript:void(0)">Element</a></li>
+					</ol>
+                </div>
+
+
+
+
+
+                
+</div>
+</div>
+
+@endsection
